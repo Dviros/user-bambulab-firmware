c412b08491aef2ecfd65324ac2e96e55e7891401
Dvir S <dviros@me.com>
2024-07-08 14:19:14 -0400

    Update bambu_firmware_ota.py

--- scripts/bambu_firmware_ota.py
+++ scripts/bambu_firmware_ota.py
@@ -1,8 +1,7 @@
-import os
-import requests
-import json
-from typing import Optional, List, Dict
-from github import Github, InputGitAuthor, GithubException
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class BambuLabOTA:
     def __init__(self):
@@ -35,84 +34,53 @@ class BambuLabOTA:
             response.raise_for_status()
             self.access_token = response.cookies.get("token")
             if response.status_code == 200 and not response.json().get("tfaKey"):
-                print("BambuLab Cloud Login Successful")
+                logging.info("BambuLab Cloud Login Successful")
                 self.get_user_devices()
             else:
-                print("BambuLab Cloud Login Failed")
+                logging.error("BambuLab Cloud Login Failed")
         except requests.RequestException as e:
-            print(f"An error occurred during login: {e}")
+            logging.error(f"An error occurred during login: {e}")
 
     def get_user_devices(self):
         try:
             response = requests.get(f"{self.api_url}/v1/iot-service/api/user/bind", headers={"authorization": f"Bearer {self.access_token}"})
             response.raise_for_status()
-            devices = response.json().get("devices", [])
-            self.select_device(devices)
+            devices = response.json().get("data", [])
+            if devices:
+                self.device_id = devices[0].get("device_id")
+                logging.info(f"Device ID: {self.device_id}")
+                self.get_device_firmware()
+            else:
+                logging.warning("No devices found.")
         except requests.RequestException as e:
-            print(f"An error occurred while getting bound devices: {e}")
+            logging.error(f"An error occurred while fetching user devices: {e}")
 
-    def select_device(self, devices: List[Dict]):
-        if not devices:
-            print("No devices found.")
+    def get_device_firmware(self):
+        if not self.device_id:
+            logging.warning("Device ID is not set.")
             return
-        device_list_str = ", ".join(f"{i+1}. {d['dev_id']}" for i, d in enumerate(devices))
-        #print(f"List of Binded Devices: {device_list_str}")
         try:
-            selected_index = 0 # int(input("Please select a device index: ")) - 1
-            self.device_id = devices[selected_index]["dev_id"]
-            self.get_device_version()
-        except (IndexError, ValueError):
-            print("Invalid index selected.")
-
-    def get_device_version(self):
-        try:
-            response = requests.get(f"{self.api_url}/v1/iot-service/api/user/device/version?dev_id={self.device_id}", headers={"authorization": f"Bearer {self.access_token}"})
+            response = requests.get(f"{self.api_url}/v1/iot-service/api/device/firmware?device_id={self.device_id}", headers={"authorization": f"Bearer {self.access_token}"})
             response.raise_for_status()
-            device_version_info = response.json()
-            printer_name, firmware_optional = self.construct_firmware_optional(device_version_info)
+            firmware_data = response.json().get("data", {})
+            printer_name, firmware_optional = self.process_firmware_data(firmware_data)
             self.compare_and_create_pull_request(printer_name, firmware_optional)
         except requests.RequestException as e:
-            print(f"An error occurred while getting the device version: {e}")
+            logging.error(f"An error occurred while fetching device firmware: {e}")
 
-    def construct_firmware_optional(self, device_version_info: Dict) -> (str, Dict): # type: ignore
-        device_id_map = {
-            "00M": "X1C",
-            "00W": "X1",
-            "01S": "P1P",
-            "01P": "P1S",
-            "030": "A1_MINI",
-            "039": "A1"
-        }
-        printer_name = next((name for prefix, name in device_id_map.items() if self.device_id.startswith(prefix)), "Unknown")
-        
-        firmware_info = device_version_info["devices"][0]["firmware"][0]
+    def process_firmware_data(self, firmware_data):
+        printer_name = firmware_data.get("name", "unknown_printer")
         firmware_optional = {
-            "user_id": "0",
-            "upgrade": {
-                "sequence_id": "0",
-                "command": "upgrade_history",
-                "src_id": 2,
-                "firmware_optional": {
-                    "firmware": {
-                        "version": firmware_info["version"],
-                        "url": firmware_info["url"],
-                        "force_update": False,
-                        "description": firmware_info["description"],
-                        "status": "release"
-                    },
-                    "ams": [] if self.device_id.startswith(("030", "039")) else [{
-                        "dev_model_name": "BL-A001",
-                        "address": 0,
-                        "device_id": "",
-                        "firmware": [{
-                            "version": "00.00.06.40",
-                            "force_update": False,
-                            "url": "https://public-cdn.bambulab.com/upgrade/device/BL-A001/00.00.06.40/product/ams-ota_v00.00.06.40-20230906131441.json.sig",
-                            "description": "",
-                            "status": "testing"
-                        }],
-                        "firmware_current": None
-                    }]
+            "firmware_optional": {
+                "stable": {
+                    "device": [
+                        {
+                            "name": printer_name,
+                            "version": firmware_data.get("version", "unknown_version"),
+                            "status": "stable"
+                        }
+                    ],
+                    "firmware_current": firmware_data.get("current_firmware")
                 }
             }
         }
@@ -128,19 +96,19 @@ class BambuLabOTA:
             old_content = contents.decoded_content.decode("utf-8")
 
             if new_content == old_content:
-                print("No changes detected in the firmware optional JSON.")
+                logging.info("No changes detected in the firmware optional JSON.")
                 return
             else:
-                print("Changes detected, creating a pull request.")
+                logging.info("Changes detected, creating a pull request.")
         except GithubException:
-            print("File does not exist, creating a new one.")
+            logging.info("File does not exist, creating a new one.")
 
         branch_name = "schedule-update"
         try:
             main_ref = repo.get_git_ref("heads/main")
             repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=main_ref.object.sha)
         except GithubException as e:
-            print(f"Error creating branch: {e}")
+            logging.error(f"Error creating branch: {e}")
 
         try:
             repo.create_file(
@@ -157,9 +125,9 @@ class BambuLabOTA:
                 head=branch_name,
                 base="main"
             )
-            print("Pull request created successfully.")
+            logging.info("Pull request created successfully.")
         except GithubException as e:
-            print(f"Error creating pull request: {e}")
+            logging.error(f"Error creating pull request: {e}")
 
 if __name__ == '__main__':
     BambuLabOTA()
